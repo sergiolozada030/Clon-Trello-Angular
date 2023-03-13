aacb3923203758110413e5d88f07c5de2f6915d6
Sergio Andrés Lozada Perea <sergio.lozada@pragma.com.co>
2023-03-13 15:19:34 -0500

    se agregan clases dinamicas

--- src/app/components/btn/btn.component.ts
+++ src/app/components/btn/btn.component.ts
@@ -6,8 +6,16 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class BtnComponent implements OnInit {
   @Input() buttonType: 'submit' | 'reset' | 'button' = 'button';
+  @Input() color: string = 'success';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  get colors() {
+    return {
+      'bg-success': this.color === 'success',
+      'hover:bg-green-600': this.color === 'success',
+    };
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+const { green } = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      success: '#5AAC44',
+      primary: colors.blue[200]
+     }
+    },
   },
+ 
   plugins: [
     require('@tailwindcss/forms'),
   ],
